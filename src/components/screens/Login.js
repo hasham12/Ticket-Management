efc7a9302c40f0804e87ad7563d5f0adc4fc348f
Hasham Ahmed <hashamahmed012@gmail.com>
2020-10-02 07:10:47 -0700

    login Api

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import axios from 'axios'
 
 export default class Login extends React.Component{
     constructor(){
         super()
+    }
+
+    state = {
+      username: '',
+      password: '',
+    };
+
+    handleSubmit = event => {
+      const data = {
+        username : this.state.username,
+        password : this.state.password,
+      }
+      console.log(data)
+      axios.post('http://localhost:51282/api/user',data).then(res => {
+        console.log(res);
+        console.log(res.data);
+        window.location = '/Home'
+      }).catch(err => {
+        alert(err);
+      })
+    }
+
+
+    handleUsername = event => {
+      console.log('username', event.target.value);
+      this.setState({
+        username: event.target.value
+      })
+    }
+
+    handlePassword = event => {
+      console.log('password', event.target.value);
+      this.setState({
+        password: event.target.value
+      })
     }
 
     render(){
@@ -13,18 +49,20 @@ export default class Login extends React.Component{
               <Header as='h2' color='teal' textAlign='center'>
                 <Image src='/logo.png' /> Log-in to your account
               </Header>
-              <Form size='large'>
+              <Form size='large' onSubmit ={this.handleSubmit}>
                 <Segment stacked>
-                  <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                  <Form.Input fluid icon='user' iconPosition='left' name='username' placeholder='E-mail address' onChange={this.handleUsername} />
                   <Form.Input
                     fluid
                     icon='lock'
                     iconPosition='left'
                     placeholder='Password'
                     type='password'
+                    name = 'password'
+                    onChange ={this.handlePassword}
                   />
         
-                  <Button color='teal' fluid size='large'>
+                  <Button type='submit' color='teal' fluid size='large'>
                     Login
                   </Button>
                 </Segment>
